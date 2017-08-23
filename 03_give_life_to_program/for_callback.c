f2e9902d6a67150512a4fdec632fc6dbd91478fe
Wei Yang <richard.weiyang@gmail.com>
2017-08-23 16:12:10 +0800

    Use callback to differentiate two ParkPlace

--- 03_give_life_to_program/for_callback.c
+++ 03_give_life_to_program/for_callback.c
@@ -2,14 +2,27 @@
 
 struct ParkPlace {
 	int status[10];
+	void (*action)(int i);
 };
+
+void place1_action(int i)
+{
+	printf("Get place (%d) in Lujiazui\n", i);
+}
+
+void place2_action(int i)
+{
+	printf("Place(%d) is reserved at IAPM \n", i);
+}
 
 struct ParkPlace place1 = {
 	.status = {1, 1, 1, 1, 0, 1, 0, 1, 0, 1},
+	.action = place1_action,
 };
 
 struct ParkPlace place2 = {
 	.status = {1, 0, 0, 1, 0, 1, 0, 1, 0, 1},
+	.action = place2_action,
 };
 
 void parking(struct ParkPlace *p)
@@ -19,7 +32,7 @@ void parking(struct ParkPlace *p)
 	for (i = 0; i < 10; i++) {
 		if (p->status[i] == 0) {
 			p->status[i] = 1;
-			printf("Park in %d place\n", i);
+			p->action(i);
 			break;
 		}
 	}
